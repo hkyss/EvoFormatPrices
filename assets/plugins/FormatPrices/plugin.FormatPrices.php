a48f99f29f453dcb76b95c21109522a7d760958b
hkyss <hkyss.off.dev@gmail.com>
2021-05-18 13:52:11 +0300

    init

---
+++ assets/plugins/FormatPrices/plugin.FormatPrices.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Format prices
+ *
+ * plugin
+ *
+ * @category        plugin
+ * @version         0.1
+ * @author          hkyss
+ * @documentation   empty
+ * @lastupdate      18.05.2021
+ * @internal    	@modx_category Resources
+ * @internal    	@events OnDocFormSave
+ * @internal    	@properties &tvs=Наименования TV-параметров;string;price &templates=Шаблоны;string;9
+ *
+ */
+
+if(!defined('MODX_BASE_PATH')) die('What are you doing? Get out of here!');
+
+if(!function_exists('format')) {
+  function format($value) {
+    $value = trim($value);
+
+    if(!empty($value)) {
+      if(strpos($value, ',') !== false) {
+        $value = str_replace(',','.',$value);
+      }
+
+      $value = str_replace(',','.',sprintf("%01.2f", $value));
+    }
+
+    //$value = round((float)$value, 2);
+
+    return $value;
+  }
+}
+
+global $template;
+
+include_once MODX_BASE_PATH.'assets/lib/MODxAPI/modResource.php';
+$modResource = new modResource($modx);
+
+$template = !empty($template) ? $template : $doc['template'];
+$templates = !empty($templates) ? explode(',',$templates) : array();
+
+if($modx->event->name == 'OnDocFormSave' && array_search($template,$templates) !== false) {
+  $modResource->edit((int)$id);
+
+  if(!empty($tvs)) {
+    $tvs = explode(',',$tvs);
+
+    foreach($tvs as $item) {
+      ${$item} = format($modResource->get($item));
+      $modResource->set($item, ${$item});
+    }
+  }
+
+  $modResource->save(false,false);
+  $modResource->close();
+}
